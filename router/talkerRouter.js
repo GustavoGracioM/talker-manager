80b9709dd38ce92c3f2faf44180d0cf050238937
gracio <gustavogracio@hotmail.com>
2022-06-11 15:03:32 -0300

    Requisito 2 completo

--- router/talkerRouter.js
+++ router/talkerRouter.js
@@ -3,11 +3,26 @@ const fs = require('fs');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+const getTalker = (req, res, next) => {
   const response = fs.readFileSync('./talker.json');
   if (!response) return res.status(200).json([]);
   const talker = JSON.parse(response);
+  req.talker = talker;
+  next();
+};
+
+router.get('/', getTalker, (req, res) => {
+  const { talker } = req;
+  console.log(talker);
   res.status(200).json(talker);
+});
+
+router.get('/:id', getTalker, (req, res) => {
+  const { talker } = req;
+  const { id } = req.params;
+  const x = talker.find((t) => t.id === Number(id));
+  if (!x) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(x);
 });
 
 module.exports = router;
